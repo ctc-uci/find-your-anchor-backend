a59e964ed8e9435b32d94540306bf8bc6fb24b05
Ashley Liu <aslorange61@gmail.com>
2022-02-04 11:25:03 -0800

    add boxForm route

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const boxFormRouter = require('./routes/boxForm');
 require('dotenv').config();
 
 // Uncomment this when we start querying the database
 // const {db} = require('./config');
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const app = express();
+// body parser middleware
+// app.use(express.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,6 +21,9 @@ app.use(
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
   }),
 );
+
+// Routes
+app.use('/boxForm', boxFormRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
