ebb4198761f283ad48cf42c85d013b2e7d850d25
meghakak <meghakak1@gmail.com>
2022-02-05 15:31:25 -0800

    Fix CORS issue.

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const PORT = process.env.PORT || 3001;
 app.use(
   cors({
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
+    credentials: true,
   }),
 );
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
