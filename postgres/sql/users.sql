07c0efb2c18c9360c7689c93a295cdf7277d099c
zoyahajee@gmail.com <zoyahajee@gmail.com>
2022-02-11 11:21:29 -0800

    added users.sql

---
+++ postgres/sql/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS public."Users"
+(
+    first_name text COLLATE pg_catalog."default",
+    last_name text COLLATE pg_catalog."default",
+    role text COLLATE pg_catalog."default",
+    email text COLLATE pg_catalog."default",
+    user_id text COLLATE pg_catalog."default"
+)
