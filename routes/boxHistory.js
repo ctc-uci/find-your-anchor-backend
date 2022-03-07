a62fcf87d08f38cc9e8059d8107c09967ae4e09e
Zubair Sidhu <sidhuzubair@gmail.com>
2022-03-06 21:29:25 -0800

    Move updateBox query to boxHistoryService

--- routes/boxHistory.js
+++ routes/boxHistory.js
@@ -1,50 +1,16 @@
 const express = require('express');
+const { db } = require('../config');
+const {
+  getBoxByID,
+  getBoxesWithStatusOrPickup,
+  updateBox,
+} = require('../services/boxHistoryService');
 
 const boxRouter = express();
-const pgp = require('pg-promise')({});
-const { db } = require('../config');
-
-const cn = `postgresql://${process.env.REACT_APP_DATABASE_USER}:${process.env.REACT_APP_DATABASE_PASSWORD}@${process.env.REACT_APP_DATABASE_HOST}:${process.env.REACT_APP_DATABASE_PORT}/${process.env.REACT_APP_DATABASE_NAME}?ssl=true`; // For pgp
-
-const database = pgp(cn);
-
 boxRouter.use(express.json());
 
 const SQLQueries = {
-  UpdateBox: (
-    status,
-    boxHolderName,
-    boxHolderEmail,
-    zipCode,
-    generalLocation,
-    message,
-    changesRequested,
-    rejectionReason,
-    messageStatus,
-    launchedOrganically,
-  ) =>
-    `UPDATE "Box_History" SET
-        box_id = $(boxID)
-        ${status !== undefined ? ', status = $(status)' : ''}
-        ${boxHolderName !== undefined ? ', boxholder_name = $(boxHolderName)' : ''}
-        ${boxHolderEmail !== undefined ? ', boxholder_email = $(boxHolderEmail)' : ''}
-        ${zipCode !== undefined ? ', zip_code = $(zipCode)' : ''}
-        ${generalLocation !== undefined ? ', general_location = $(generalLocation)' : ''}
-        ${message !== undefined ? ', message = $(message)' : ''}
-        ${changesRequested !== undefined ? ', changes_requested = $(changesRequested)' : ''}
-        ${rejectionReason !== undefined ? ', rejection_reason = $(rejectionReason)' : ''}
-        ${messageStatus !== undefined ? ', message_status = $(messageStatus)' : ''}
-        ${
-          launchedOrganically !== undefined ? ', launched_organically = $(launchedOrganically)' : ''
-        }
-        WHERE
-          box_id = $(boxID)`,
   Return: 'Returning *',
-  GetBoxes: (pickup) =>
-    `SELECT * FROM "Box_History" WHERE
-      ($(status) = '' OR status = $(status))
-      ${pickup ? 'AND pickup = $(pickup)' : ''}`,
-  GetBox: 'SELECT * FROM "Box_History" WHERE box_id = $1',
   CopyBoxInfoToAnchorBox:
     'UPDATE "Anchor_Box" SET message = $1, zip_code = $2, picture = $3, general_location = $4, date=$5, launched_organically=$6 WHERE box_id = $7',
   ApproveBoxInBoxHistory:
@@ -67,32 +33,18 @@ boxRouter.put('/update', async (req, res) => {
       messageStatus,
       launchedOrganically,
     } = req.body;
-    const response = await database.query(
-      SQLQueries.UpdateBox(
-        status,
-        boxHolderName,
-        boxHolderEmail,
-        zipCode,
-        generalLocation,
-        message,
-        changesRequested,
-        rejectionReason,
-        messageStatus,
-        launchedOrganically,
-      ) + SQLQueries.Return,
-      {
-        status,
-        boxID,
-        boxHolderName,
-        boxHolderEmail,
-        zipCode,
-        generalLocation,
-        message,
-        changesRequested,
-        rejectionReason,
-        messageStatus,
-        launchedOrganically,
-      },
+    const response = await updateBox(
+      status,
+      boxID,
+      boxHolderName,
+      boxHolderEmail,
+      zipCode,
+      generalLocation,
+      message,
+      changesRequested,
+      rejectionReason,
+      messageStatus,
+      launchedOrganically,
     );
     res.status(200).send(response);
   } catch (err) {
@@ -106,18 +58,18 @@ boxRouter.get('/', async (req, res) => {
     let { status, pickup } = req.query;
     status = status === undefined ? '' : status;
     pickup = pickup === undefined ? '' : pickup;
-    const allBoxes = await database.query(SQLQueries.GetBoxes(pickup), { status, pickup });
+    const allBoxes = await getBoxesWithStatusOrPickup(status, pickup);
     res.status(200).send(allBoxes);
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
-// get a box
+// get a box by id
 boxRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
   try {
-    const box = await db.query(SQLQueries.GetBox, [id]);
+    const box = await getBoxByID(id);
     if (box.length === 0) {
       res.status(400).send(box);
     } else {
