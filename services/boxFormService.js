41b5a8a609bd8e399f785bc9afa0a2ecafd267ea
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2022-03-08 14:37:29 -0600

    Refactor backend queries (#22)
    * Create PR for #21
    
    * Move updateBox query to boxHistoryService
    
    * Move approveBoxInBoxHistory and copyBoxInfoToAnchorBox to boxHistoryService
    
    * Move boxForm queries to boxFormService
    
    * Add missing ile
    
    * Update query to reflect changes in Anchor Box table
    
    Co-authored-by: ZubairSidhu <ZubairSidhu@users.noreply.github.com>
    Co-authored-by: Zubair Sidhu <sidhuzubair@gmail.com>

---
+++ services/boxFormService.js
@@ -0,0 +1,56 @@
+const pgp = require('pg-promise')({});
+
+const cn = `postgresql://${process.env.REACT_APP_DATABASE_USER}:${process.env.REACT_APP_DATABASE_PASSWORD}@${process.env.REACT_APP_DATABASE_HOST}:${process.env.REACT_APP_DATABASE_PORT}/${process.env.REACT_APP_DATABASE_NAME}?ssl=true`; // For pgp
+const db = pgp(cn);
+
+const findBoxId = async (id) => {
+  let res = null;
+  try {
+    res = await db.query(
+      `SELECT box_id FROM "Anchor_Box"
+      WHERE box_id = $1`,
+      [id],
+    );
+  } catch (err) {
+    throw new Error(err.message);
+  }
+  return res;
+};
+
+const createAnchorBox = async (
+  boxNumber,
+  message,
+  zipCode,
+  picture,
+  boxLocation,
+  date,
+  launchedOrganically,
+  additionalComments,
+) => {
+  let res = null;
+  try {
+    res = await db.query(
+      `INSERT INTO "Anchor_Box"
+        (box_id, message,
+        zip_code, picture, general_location,
+        date, launched_organically, additional_comments)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING *;`,
+      [
+        boxNumber,
+        message,
+        zipCode,
+        picture,
+        boxLocation,
+        date,
+        launchedOrganically,
+        additionalComments,
+      ],
+    );
+  } catch (err) {
+    throw new Error(err.message);
+  }
+  return res;
+};
+
+module.exports = { findBoxId, createAnchorBox };
