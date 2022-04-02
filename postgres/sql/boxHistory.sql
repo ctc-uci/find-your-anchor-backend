d5c7efad3bf1afeaeb45d9d3f1372dec94db3f74
meghakak <meghakak1@gmail.com>
2022-04-01 23:17:24 -0700

    Add boxHistory.sql.

---
+++ postgres/sql/boxHistory.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS public."Box_History"
+(
+    transaction_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    box_id integer NOT NULL,
+    message character varying COLLATE pg_catalog."default",
+    boxholder_email character varying COLLATE pg_catalog."default",
+    boxholder_name character varying COLLATE pg_catalog."default",
+    general_location character varying COLLATE pg_catalog."default",
+    picture character varying COLLATE pg_catalog."default",
+    approved boolean,
+    status character varying COLLATE pg_catalog."default",
+    pickup boolean,
+    image_status character varying(255) COLLATE pg_catalog."default",
+    changes_requested character varying COLLATE pg_catalog."default",
+    rejection_reason character varying COLLATE pg_catalog."default",
+    message_status character varying COLLATE pg_catalog."default",
+    zip_code character varying COLLATE pg_catalog."default",
+    date character varying COLLATE pg_catalog."default",
+    launched_organically boolean,
+    image_status character varying(255) COLLATE pg_catalog."default",
+    CONSTRAINT "Box_History_pkey1" PRIMARY KEY (transaction_id)
+);
