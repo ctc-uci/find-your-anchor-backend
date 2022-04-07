568b53f3ffde34e568334bc53e2ea2d34c4e40f1
Ashley Liu <aslorange61@gmail.com>
2022-04-06 23:53:32 -0700

    remove nit

--- routes/anchorBox.js
+++ routes/anchorBox.js
@@ -73,7 +73,6 @@ anchorBoxRouter.post('/box', async (req, res) => {
   }
 });
 
-// TODO: Convert this route to use pg-promise's multi-query string feature: http://vitaly-t.github.io/pg-promise/Database.html#multi
 anchorBoxRouter.post('/boxes', async (req, res) => {
   try {
     const response = await createAnchorBoxes(req.body);
