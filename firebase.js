3d0d1dc1e44c476f6918220c8c1f0742f8503116
Zubair Sidhu <sidhuzubair@gmail.com>
2022-04-09 20:35:56 -0700

    Use env vars for firebase config

--- firebase.js
+++ firebase.js
@@ -2,8 +2,12 @@ const admin = require('firebase-admin');
 
 require('dotenv').config();
 
-const credentials = require('./firebase-adminsdk.json');
-
-admin.initializeApp({ credential: admin.credential.cert(credentials) });
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+});
 
 module.exports = admin;
