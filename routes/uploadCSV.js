a390a4c5a9e515e6da640eca3e03ab6193de0def
Ashley Liu <aslorange61@gmail.com>
2022-05-14 11:17:44 -0700

    add comment

--- routes/uploadCSV.js
+++ routes/uploadCSV.js
@@ -9,6 +9,7 @@ router.post('/', upload.single('file'), async (req, res) => {
   try {
     const resp = await uploadCSVService.parseCSV(req);
 
+    // this is to measure how much memory the csv file takes up
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
     console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 
